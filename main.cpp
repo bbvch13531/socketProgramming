b96c3784787626cdfb9887921fabd89ed41c5984
bbvch13531 <bbvch13531@gmail.com>
2018-09-27 00:44:18 +0900

    Add socket function description

--- main.cpp
+++ main.cpp
@@ -1,7 +1,36 @@
+// https://www.joinc.co.kr/w/Site/Network_Programing/Documents/Socket_Sample
+// https://github.com/openbsd/src/blob/master/sys/sys/socket.h
+// http://pubs.opengroup.org/onlinepubs/7908799/xns/socket.html
 #include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h> 
+#include <cstdlib>
+#include <cstdio>
+#include <cstring>
+#include <unistd.h>
+#define PORT 8080
+
 using namespace std;
 
 int main(void){
+    int server_sock, client_sock;
+    int state, client_len;
+
+    /*
+    // int socket(int domain, int type, int protocol);
+    // 서버 소켓 생성
+    // PF_INET : 인터넷 프로토콜 체계, # define AF_INET 2로 UDP, TCP 프로토콜 이용할 때 사용
+    // SOCK_STREAM : 스트림 방식의 소켓 (TCP) 생성
+    // 0 : 구체적인 프로토콜을 정의할 때 사용
+    */
+    server_sock = socket(PF_INET, SOCK_STREAM, 0); 
+    
+    /*
+    // int connect(int socket, const struct sockaddr *address,
+    // socklen_t address_len);
+    // 
+    */
+
     cout<<"hello socket" ;
     return 0;
 }
